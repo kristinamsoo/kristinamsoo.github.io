461cd20acece6d778fe99e563842a0a205e4f161
kristinamsoo <kristinamsoo1@gmail.com>
2025-01-26 15:06:08 +0200

    updated 26.01

---
+++ Lesson6/scripts.js
@@ -0,0 +1,12 @@
+//Single line cmment
+/* 
+Multi line comment
+Multi line comment
+*/
+
+//Muutuja - Variable
+// syntax (var) VariableName = value;
+var x = 10; // 1995 to 2015
+let y = 20; // 2015 ->
+const pi = 3.14; // Constant
+
